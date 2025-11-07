42179e7b75232247009280c0c45effc0817d75cb
pramadha <pramslabs@gmail.com>
2025-11-07 11:17:41 +0700

    Add multi-target pino logger (console, file, mongodb)

--- src/logs/pino.js
+++ src/logs/pino.js
@@ -1,55 +1,11 @@
-import pino from "pino";
-import "dotenv/config";
-
-// export const logger = pino({
-//   enabled: process.env.LOG_ENABLED === "true",
-//   level: "trace",
-//   formatters: {
-//     level: (label) => {
-//       return {
-//         label: label.toUpperCase(),
-//         timestamp: new Date().toISOString(),
-//       };
-//     },
-//   },
-// });
-
-// export const logger = pino({
-//   enabled: process.env.LOG_ENABLED === "true",
-//   level: "trace",
-//   transport: {
-//     target: "pino-pretty",
-//     options: {
-//       colorize: true,
-//       translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
-//       ignore: "pid,hostname",
-//     },
-//   },
-// });
-
-// export const logger = pino({
-//   enabled: process.env.LOG_ENABLED === "true",
-//   level: "trace",
-//   transport: {
-//     target: "pino-pretty",
-//     options: {
-//       destination: "./logs/file/app.log",
-//       mkdir: true,
-//       colorize: false,
-//       translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
-//       ignore: "pid,hostname",
-//     },
-//   },
-// });
-
 export const logger = pino({
   enabled: process.env.LOG_ENABLED === "true",
-  level: "trace",
+  level: process.env.LOG_LEVEL || "info",
   transport: {
     targets: [
       {
         target: "pino-pretty",
-        level: "trace",
+        level: "info",
         options: {
           colorize: true,
           translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
@@ -58,23 +14,11 @@ export const logger = pino({
       },
       {
         target: "pino-pretty",
-        level: "trace",
+        level: "info",
         options: {
           destination: "./logs/file/app.log",
-          colorize: false,
-          translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
           mkdir: true,
-          ignore: "pid,hostname",
-        },
-      },
-      {
-        target: "pino-mongodb",
-        level: "error",
-        options: {
-          uri: "mongodb://127.0.0.1:27017/",
-          database: "latihan",
-          collection: "log-collection",
-          colorize: true,
+          colorize: false,
           translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
           ignore: "pid,hostname",
         },
